4ea28c950c83931ec68ca4cf7d8a4214f997b617
Amalia Simion <amalia.simion@stud.fils.upb.ro>
2024-11-09 10:53:43 +0200

    update template

--- src/connection.rs
+++ src/connection.rs
@@ -4,14 +4,11 @@ struct CityInfo {
     // TODO: define elements in the structure
 }
 
-/// Method that is handling the request to the openweather api,
-/// parsing the response
-///
-/// IP: 34.116.205.113
-/// Port: 3000
+/// Method that is handling the request to the OpenWeather api
+/// and parsing the response
 ///
 /// Returns weather details about a certain city
-pub fn get_temperature(city: String) {
+pub fn get_data(city: String) {
     match reqwest::blocking::get("") {
         Ok(response) => {
             // Check status code
